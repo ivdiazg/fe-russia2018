94ae4a3109a57712918f04868394d3223880ffcd
DOM_BCI_PROD\EXIDIGO <idigonza@everis.com>
2018-04-04 14:49:02 -0300

    incluye template y banderas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { SidebarModule } from './components/sidebar/sidebar.module';
+import { NavbarModule } from './shared/navbar/navbar.module';
+import { FooterModule } from './shared/footer/footer.module';
 
+// MATERIAL
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule, MatCardModule } from '@angular/material';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    FormsModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatTabsModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SidebarModule,
+    NavbarModule,
+    FooterModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: []
 })
 export class AppModule { }
---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface RouteInfo {
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+}
+
+export const ROUTES: RouteInfo[] = [
+  { path: 'home', title: 'Home', icon: 'ti-panel', class: '' },
+  // { path: 'client', title: 'Cliente', icon: 'ti-user', class: '' },
+  // { path: 'category', title: 'Categoria', icon: 'ti-view-list-alt', class: '' },
+  // { path: 'product', title: 'Producto', icon: 'ti-tag', class: '' },
+  // { path: 'sale', title: 'Venta', icon: 'ti-shopping-cart', class: '' },
+  // { path: 'icons', title: 'Icons', icon: 'ti-pencil-alt2', class: '' },
+  // { path: 'maps', title: 'Maps', icon: 'ti-map', class: '' },
+  // { path: 'notifications', title: 'Notifications', icon: 'ti-bell', class: '' },
+  // { path: 'upgrade', title: 'Upgrade to PRO',  icon:'ti-export', class: 'active-pro' },
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+
+  constructor() { }
+
+  public menuItems: any[];
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+
+  isNotMobileMenu() {
+    // if ($(window).width() > 991) {
+    //   return false;
+    // }
+    return true;
+  }
+
+}
